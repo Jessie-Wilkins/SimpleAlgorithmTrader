1b09a9a7347f3744d6afb9b43dcc1133a154f48e
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2016-10-22 21:26:23 -0500

    Added getters and setters to Stock class and partial function to the run method.

--- src/AlgorithmTrader.java
+++ src/AlgorithmTrader.java
@@ -1,11 +1,43 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.*;
 
 public class AlgorithmTrader {
 	
-	public ArrayList<Stock> ReadInputData(Scanner f, Stock s) {
+	public ArrayList<Stock> ReadInputData(String fName) {
+		Scanner fCount = new Scanner(fName);
+		Scanner fRead = new Scanner(fName);
+		String dummyString;
+		int i=0;
+		ArrayList<Stock> a = new ArrayList<Stock>(100);
+		int count = 0;
 		
-		ArrayList<Stock> a = null;
+		while(fCount.hasNext()) {
+			dummyString = fCount.nextLine();
+			count++;
+		}
+		
+		String [] array = new String[count];
+		
+		while(fRead.hasNext()) {
+			array[i] = fRead.nextLine();
+			i++;
+		}
+		for(i=0; i<array.length; i++) {
+			String [] stockRecords = array[i].split("[,]");
+			
+			Stock s = new Stock();			
+			a.add(s);
+			
+			a.get(i).setTimeStamp(stockRecords[0]);
+			a.get(i).setOpeningPrice(Double.parseDouble(stockRecords[1]));
+			a.get(i).setHighestPrice(Double.parseDouble(stockRecords[2]));
+			a.get(i).setLowestPrice(Double.parseDouble(stockRecords[3]));
+			a.get(i).setClosingPrice(Double.parseDouble(stockRecords[4]));
+		}
+		fCount.close();
+		fRead.close();
+		
 		return a;
 	}
 	
@@ -14,7 +46,7 @@ public class AlgorithmTrader {
 
 		for(int i = num; i<num+5; i++) {
 			
-			if(s.get(i+1).getCurrentPrice() > s.get(i).getCurrentPrice()){
+			if(s.get(i+1).getClosingPrice() > s.get(i).getClosingPrice()){
 				b = true;	
 			}
 			else {
@@ -28,7 +60,7 @@ public class AlgorithmTrader {
 	public boolean ExitStrategy(ArrayList<Stock> s, int num) {
 		boolean b;
 		
-		if(s.get(num+1).getCurrentPrice()-s.get(num).getCurrentPrice() > .12*s.get(num).getCurrentPrice()){
+		if(s.get(num+1).getClosingPrice()-s.get(num).getClosingPrice() > .12*s.get(num).getClosingPrice()){
 			b = true;
 		}
 		else{
@@ -37,7 +69,31 @@ public class AlgorithmTrader {
 		return b;
 	}
 	
-	public void Run() {
+	public void Run(String fName, PrintWriter f) {
+		
+		ArrayList<Stock> stockFileData = new ArrayList<Stock>();
+		
+		stockFileData = ReadInputData(fName);
+		
+		boolean entryBool;
+		boolean exitBool;
+		
+		for(int i =0; i<stockFileData.size(); i=i+5) {
+			entryBool= EntryStrategy(stockFileData, i);
+			exitBool = ExitStrategy(stockFileData, i);
+			double heldStock;
+			double revenue;
+			
+			if(entryBool) {
+				heldStock = 10000*stockFileData.get(i+4).getClosingPrice();
+			}
+			
+			if(exitBool) {
+				revenue = 10000*stockFileData.get(i+4).getClosingPrice();
+			}
+			
+		}
+		
 	
 	}
 }
--- src/Stock.java
+++ src/Stock.java
@@ -1,112 +1,47 @@
-import java.util.Random;			//Import Random Generator
+
 import java.io.*;
 
 public class Stock {
 	//Variables
-	private String companyName;		//String object holding name of Company
-	private String companySymbol;	//String object holding Stock Symbol
+	private String timeStamp;			
 	private String outputFileName;
 	private String inputFileName;
-	private double currentPrice;	//Double holding Current Price of Stock
-	private double nextPrice;		//Double holding Next Price of Stock
-	private double randomNumber;	//Double holding random Number
+	private double openingPrice;	//Double holding Next Price of Stock
+	private double highestPrice;
+	private double lowestPrice;
+	private double closingPrice;
+	private double volumeOfStocks;
 	
 	/**
 	 * This sets the private variables to the default
 	 */
 	public Stock() {
-		//Assigns default values for private variables
-		companyName = "Microsoft";	//Assigns company name as default
-		companySymbol = "MSFT";		//Assigns company symbol as default
-		currentPrice = 46.87;		//Assigns current price as default
-		nextPrice = 46.87;			//Assigns next price as default
+		
 	}//Closes Stock Method
+	
 	/**
-	 * Determines if the default values should be used
-	 * @param cN
-	 * @param cS
-	 * @param cP
-	 */
-	public void WhichDefaultValue(String cN, String cS, double cP) {
-		//This sets the name, symbols, and current price if they do not equal the default value keywords
-		if(!(cN.contentEquals("NONE")) && !(cS.contentEquals("NA")) && cP != 0.0) {
-			setName(cN);
-			setSymbol(cS);
-			setCurrentPrice(cP); 
-		}//Closes if statement
-		
-	}//Closes WhichDefaultValue Method
-	/**
-	 * Sets the private variables to the given values
-	 * @param n
-	 * @param s
-	 * @param cP
-	 * @param nP
-	 */
-	public Stock(String n, String s, double cP, double nP) {
-		//Stock Method that creates user instance of Stock details
-		companyName = n;
-		companySymbol = s;
-		currentPrice = cP;
-		nextPrice = nP;
-	}//Closes public Stock Method
-	/**
-	 * This sets the company name to the given string
+	 * This sets the time stamp to the given string
 	 * @param n
 	 */
-	public void setName(String n){
+	public void setTimeStamp(String t){
 		//Assigns the name to the private variable
-		companyName = n;
+		timeStamp = t;
 	}
-	/**
-	 * This sets the company symbol to the given string
-	 * @param s
-	 */
-	public void setSymbol(String s) {
-		//Assigns the symbol to the private variable
-		companySymbol = s;
+	public void setOpeningPrice(double o) {
+		openingPrice = o;
 	}
-	/**
-	 * This sets the current price to the given double
-	 * @param cP
-	 */
-	public void setCurrentPrice(double cP) {
-		//Error Checking for: currentPrice > 0, set the value to the variable; otherwise set to 1
-		if(cP > 0) {
-			currentPrice = cP;
-		}//Close if statement
-		
-		else {
-			currentPrice = 1;
-		}//Close if statement
-	}//Closes setCurrentPrice Method
-	/**
-	 * This sets the next price to the given double
-	 * @param nP
-	 */
-	public void setNextPrice(double nP) {
-		//Error Checking for: nextPrice > 0, set the value to the variable; otherwise set to 1
-		if(nP > 0) {
-			nextPrice = nP;
-		}//Close if statement
-		
-		else {
-			nextPrice = 1;
-		}//Close else statement
-	}//Closes setNextPrice Method
-	/**
-	 * This sets the percentage to the given double
-	 * @param p
-	 */
-	public void setPercentage(double p) {
-		//Error Checking for: randomNumber > 0, set the value to the variable; otherwise set to 1
-		if(p > 0) {
-			randomNumber = p;
-		}
-		else {
-			randomNumber = 1.00;
-		}
-	}//Closes setPercentage Method
+	public void setHighestPrice(double h) {
+		highestPrice = h;
+	}
+	public void setLowestPrice(double l) {
+		lowestPrice = l;
+	}
+	public void setClosingPrice(double c) {
+		closingPrice = c;
+	}
+	public void setVolumeOfStocks(double v) {
+		volumeOfStocks = v;
+	}
 	
 	public void SetOutputFileName(String s) {
 		outputFileName = s;
@@ -120,42 +55,26 @@ public class Stock {
 	 * This returns the company name 
 	 * @return
 	 */
-	public String getName() {
+	public String getTimeStamp() {
 		//returns the name of the company
-		return companyName;
+		return timeStamp;
 	}
-	/**
-	 * This returns the company symbol
-	 * @return
-	 */
-	public String getSymbol() {
-		//returns the name of the company
-		return companySymbol;
+	public double getOpeningPrice() {
+		return openingPrice;
 	}
-	/**
-	 * This returns the current price
-	 * @return
-	 */
-	public double getCurrentPrice() {
-		//returns the current price of the company
-		return currentPrice;
+	public double getHighestPrice() {
+		return highestPrice;
 	}
-	/**
-	 * This returns the next price 
-	 * @return
-	 */
-	public double getNextPrice() {
-		//returns the current price of the company
-		return nextPrice;
+	public double getLowestPrice() {
+		return lowestPrice;
 	}
-	/**
-	 * This returns the percentage
-	 * @return
-	 */
-	public double getPercentage() {
-		//returns the random number
-		return randomNumber;
+	public double getClosingPrice() {
+		return closingPrice;
 	}
+	public double getVolumeOfStocks() {
+		return volumeOfStocks;
+	}
+	
 	public String getOutputFileName() {
 		return outputFileName;
 	}
@@ -165,41 +84,7 @@ public class Stock {
 	/**
 	 * This changes next price according the percentage by a negative or positive number
 	 */
-	public void priceChange() {
-		//Variables
-		boolean decide;						//declares a boolean
-		double percent;						//declares a double used as the percent
-		double currentPrice;				//declares a double used as the current price
-		
-		//This declares the random generator object
-		Random randInt = new Random();
-		//This sets the percentage as a random integer between 0 and 10
-		setPercentage(randInt.nextInt(10));
-		//This sets the variable as the percentage
-		percent = getPercentage();
-		//This sets the variable as the current price
-		currentPrice = getCurrentPrice();
-		//This declares the random generator object
-		Random negOrPos = new Random();
-		//This assigns the variables as a boolean
-		decide = negOrPos.nextBoolean();
-		
-		//This sets the next price as the current price - the percentage of current price
-		//if decide is true
-		if(decide) {
-			setNextPrice(currentPrice-(currentPrice*.01*percent));
-		}//Close if statement
-		//This sets the next price as the current price + the percentage of current price
-		//if the former condition is false
-		else {
-			setNextPrice(currentPrice+(currentPrice*.01*percent));
-		}//Close else statement
-		
-	}//Closes priceChange Method
 	
-	/**
-	 * Prints Headers for each Category
-	 */
 	public void printHeaders(){
 		//Outputs the header names
 		System.out.println("STOCK\tSYMBOL\tYESTERDAY'S PRICE\tTODAY'S PRICE\tPRICE MOVEMENT\tCHANGE PERCENT");
